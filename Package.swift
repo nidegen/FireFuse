af594085ac828efc2edb893077b34a8ba1782080
Nicolas Degen <ndegen@me.com>
2021-05-31 07:16:29 +0200

    tvOS and watchOS support

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "FireFuse",
-  platforms: [.iOS("13.0"), .macOS("10.15")],
+  platforms: [.iOS("13.0"), .tvOS("13.0"), .macOS("10.15"), .watchOS("6.0")],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
@@ -14,7 +14,7 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/nidegen/Fuse", from: "0.5.0"),
+    .package(url: "https://github.com/nidegen/Fuse", from: "0.5.1"),
     .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk", from: "7.7.0"),
   ],
   targets: [
