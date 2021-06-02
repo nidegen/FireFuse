460c86a8ce9c95e73f8ebfc3bc9d29bf12271ba0
Nicolas Degen <ndegen@me.com>
2021-06-02 15:22:52 +0200

    support firebase [7.0-9.0)

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     .package(url: "https://github.com/nidegen/Fuse", from: "0.5.1"),
-    .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk", from: "7.7.0"),
+    .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk", "7.0.0"..."8.88.8"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
